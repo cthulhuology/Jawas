0ecf369a41b0b825755c6c73a1ad7faea9ca6fb8
David J. Goehrig <dave@234.100.168.192.in-addr.arpa>
2007-08-22 13:21:05 -0400

    Added config.h

---
+++ config.h
@@ -0,0 +1,13 @@
+// config.h
+//
+// Copyright (C) 2007 David J. Goehrig
+// All Rights Reserved
+
+#define DB_CONNECT_STRING "dbname=jawas user=jawas"
+#define LOG_LEVEL 2
+#define SERVER_PORT "80"
+#define TLS_SERVER_PORT "443"
+#define TLS_KEYFILE "/Users/dave/Code/Jawas/server.pem"
+#define TLS_PASSWORD "fullfathomfive"
+#define PAGE_GUARD 1
+#define CACHE_PAGES 100
