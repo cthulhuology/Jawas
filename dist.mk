7e7f1bca3d71bce08f505228247054e5af1efda0
David Goehrig <dave@sarnath.local>
2007-12-11 16:51:08 -0500

    added clear path the local repository on foriegn machine

--- dist.mk
+++ dist.mk
@@ -8,7 +8,7 @@ TARGETS +=  apu
 .PHONY: $(TARGETS)
 $(TARGETS):
 	git push $(TARGETS)
-	ssh $(USER)@$(TARGETS) "cd dev/Jawas && git pull repo"
+	ssh $(USER)@$(TARGETS) "cd dev/Jawas && git pull /usr/pub/git/jawas.git"
 
 .PHONY: dist
 dist:
