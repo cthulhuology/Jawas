84fb9185e458e0121862b14fc9ce8631cf215a49
David Goehrig <dave@sarnath.local>
2008-01-07 13:59:16 -0500

    removed chunked transfer of request

--- facebook.c
+++ facebook.c
@@ -63,7 +63,7 @@ facebook_method(str method, Headers kv, str callback)
 
 	request_headers(req,Str("Content-type"),Str("application/x-www-form-urlencded"));
 	request_headers(req,Str("User-Agent"),Str("Jawas"));
-	//request_headers(req,Str("Content-Length"),Str("%i",len(args)));
+	request_headers(req,Str("Content-Length"),Str("%i",len(args)));
 	
 	req = request_data(req,args);
 
