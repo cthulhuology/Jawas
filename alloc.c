f566b2ba310d0b2dc1f7ed3490121197e750d7a2
David Goehrig <dave@sarnath.local>
2008-01-07 15:57:50 -0500

    removed memory free to debug issue

--- alloc.c
+++ alloc.c
@@ -67,11 +67,12 @@ adopt_scratch(Scratch dst, Scratch src)
 void
 free_scratch(Scratch s)
 {
-	if (!s) return;
-	if (s->next) free_scratch(s->next);	
-	if (free_page((Page)s)) {
-		++gsi.frees;
-	}
+	debug("Pretend to free scratch!");
+//	if (!s) return;
+//	if (s->next) free_scratch(s->next);	
+//	if (free_page((Page)s)) {
+//		++gsi.frees;
+//	}
 }
 
 void
