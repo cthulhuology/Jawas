b074ff1f02d9ae4055c458e805c8eb1279bd8fd1
cthulhuology <dave@nexttolast.com>
2009-09-25 20:30:26 -0400

    Added script mime handling

---
+++ script.h
@@ -0,0 +1,12 @@
+// script.h
+//
+// Copyright (C) 2009 David J. Goehrig
+// All Rights Reserved
+//
+
+#ifndef __HAVE_SCRIPT_H__
+#define __HAVE_SCRIPT_H__
+
+int script_handler(File fc);
+
+#endif
